4098beb6fba66df1635ef315ad06f61fb1999d86
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-01-08 22:51:33 -0300

    Listas dentro de listas (iteráveis dentro de iteráveis)

---
+++ aula25.py
@@ -0,0 +1,16 @@
+salas = [
+
+    ['Maria', 'Helena'],
+
+    ['Elaine', ],
+
+    ['Luiz', 'João', 'Eduarda'],
+
+]
+
+# print(salas[2][3][2])
+
+for sala in salas:
+    print(f'A sala é {sala}')
+    for aluno in sala:
+        print(aluno)
