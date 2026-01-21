770376eed9c26358d60742d6e09e5277b5464fac
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-01-21 00:37:39 -0300

    criei um arquivo reserva por que vou fazer alterações nele e não queria perder as linhas de código

--- cpf_ex01_reserva.py
+++ cpf_ex01_reserva.py
@@ -1,7 +1,22 @@
-cpf_gerado_usuario = '46404812096'
+import re
+import sys
 
-nove_digitos = cpf_gerado_usuario[:9]
 
+
+entrada = input('CPF [464.048.120-96]: ')
+cpf_enviado_usuario = re.sub(
+    r'[^0-9]',
+    '',
+    entrada
+)
+
+entrada_e_sequencial = entrada == entrada[0] * len(entrada)
+
+if entrada_e_sequencial:
+    print('Voce enviou dados sequencias.')
+    sys.exit()
+
+nove_digitos = cpf_enviado_usuario[:9]
 contador_regressivo_1 = 10
 
 resultado_digito_1 = 0
@@ -23,8 +38,8 @@ digito_2 = digito_2 if digito_2 <= 9 else 0
 
 cpf_gerado_calculo = f'{nove_digitos}{digito_1}{digito_2}'
 
-if cpf_gerado_usuario == cpf_gerado_calculo:
-    print(f'{cpf_gerado_usuario} é valido')
+if entrada == cpf_gerado_calculo:
+    print(f'{entrada} é valido')
 else:
     print('CPF invalido')
 
