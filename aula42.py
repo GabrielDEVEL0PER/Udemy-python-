4e22ef58bfea8db7964400af5a9c2682ef28331c
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-02-12 14:16:30 -0300

    adiciona exemplo de uso de argumentos nomeados e mescla dicionários

---
+++ aula42.py
@@ -0,0 +1,45 @@
+a, b = 1, 2
+a, b = b, a
+# print(a,b)
+
+
+pessoa = {
+    'nome': 'Aline',
+    'sobrenome': 'Souza',
+}
+
+dados_pessoa = {
+    'idade': 16,
+    'altura': 1.6,
+}
+
+pessoas_completa = {**pessoa, **dados_pessoa}
+# print(pessoas_completa)
+
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2)
+# print(b1, b2)
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+# for chave, valor in dados_pessoa.items():
+#         print(chave, valor)
+
+def mostro_argumentos_nomeados(*args, **kwargs):
+    print('NÃO NOMEADOS:', args)
+
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+
+# mostro_argumentos_nomeados(nome='Joana', qlq=123)
+# mostro_argumentos_nomeados(**pessoas_completa)
+
+configuracoes = {
+    'arg1': 1,
+    'arg2': 2,
+    'arg3': 3,
+    'arg4': 4,
+
+}
+
+mostro_argumentos_nomeados(**configuracoes)
