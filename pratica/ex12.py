f44b4dbdbfc4e96f56e344722d761a8a3e699a7d
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-03-07 23:59:19 -0300

    ajusta cálculo de litros necessários e melhora apresentação das opções de compra

--- pratica/ex12.py
+++ pratica/ex12.py
@@ -1,15 +1,29 @@
 import math
 area_pintada = float(input('Digite os m² da área: '))
 
-litros_necessarios = area_pintada / 6
+litros_necessarios = (area_pintada / 6) * 1.1
 
-latas1 = math.ceil(litros_necessarios / 18)
+latas = math.ceil(litros_necessarios / 18)
+preco_latas = latas * 80
 
-preco_total1 = latas1 * 80
+galoes = math.ceil(litros_necessarios / 3.6 )
+preco_galoes = galoes * 25
 
-latas2 = math.ceil(litros_necessarios / 3.6 )
+latas_mistura = int(litros_necessarios // 18)
+resto = litros_necessarios - (latas_mistura * 18)
 
-preco_total2 = latas2 * 25
+galoes_mistura = math.ceil(resto / 3.6)
 
-print(f'Se você comprar em latas de 18 litros o preço é esse {preco_total1}')
-print(f'Se você comprar em latas de 3.6 litros o preço é esse {preco_total2}')
+preco_mistura = (latas_mistura * 80) + (galoes_mistura * 25)
+
+print("\n--- OPÇÕES DE COMPRA ---")
+
+print(f'\nApenas latas de 18L:')
+print(f'{latas} latas - R$ {preco_latas:.2f}')
+
+print(f'\nApenas galões de 3.6L:')
+print(f'{galoes} galões - R$ {preco_galoes:.2f}')
+
+print(f'\nMisturando latas e galões:')
+print(f'{latas_mistura} latas e {galoes_mistura} galões')
+print(f'Preço total: R$ {preco_mistura:.2f}')
