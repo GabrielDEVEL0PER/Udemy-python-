14f334d0d9ff4dd539c92146ad62dfed081e1d6d
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-03-17 15:50:00 -0300

    adiciona módulo aula57 e implementa funções de soma e variável

---
+++ aula57.py
@@ -0,0 +1,9 @@
+import aula57_m
+from aula57_m import soma, variavel_modulo
+
+# print('Este módulo se chama', __name__)
+
+print(aula57_m.variavel_modulo)
+print(variavel_modulo)
+print(soma(5, 3))
+print(aula57_m.soma(4, 7))
