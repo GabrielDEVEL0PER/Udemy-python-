114e552f19b5d4e43e9b46202d3e479009d7a7b2
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-03-22 02:19:36 -0300

    adiciona novos produtos com preço atualizado e ordena produtos por nome e preço

--- aula58.py
+++ aula58.py
@@ -3,6 +3,33 @@ import copy
 from dados import produtos
 
 novos_produtos = [
-    p for p in copy.deepcopy(produtos)
-
+    {**p, 'preco': round(p['preco'] * 1.1, 2)}
+    for p in copy.deepcopy(produtos)
 ]
+
+
+produtos_ordenados_por_nome = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['nome'],
+    reverse= True
+)
+
+# print(*produtos, sep='\n')
+# print()
+# print(*produtos_ordenados_por_nome, sep='\n')
+
+
+produtos_ordenados_por_preco = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['preco']
+)
+
+print(*produtos, sep='\n')
+print()
+print(*novos_produtos, sep='\n')
+
+print()
+print(*produtos_ordenados_por_nome, sep='\n')
+
+print()
+print(*produtos_ordenados_por_preco, sep='\n')
