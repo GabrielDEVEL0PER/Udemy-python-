0ea3951fd20e44461a1da183949433560d2364d4
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-03-24 07:17:58 -0300

    adiciona funções de soma e multiplicação com suporte a currying

---
+++ aula59.py
@@ -0,0 +1,15 @@
+def soma(x, y):
+    return x + y
+def multiplica(x, y):
+    return x * y
+
+def criar_funcao(funcao, *args):
+    def interna(*args_internos):
+        return funcao(*args, *args_internos)
+    return interna
+
+soma_com_cinco = criar_funcao(soma, 5)
+multiplica_por_dez = criar_funcao(multiplica, 10)
+
+print(soma_com_cinco(10))
+print(multiplica_por_dez(2))
