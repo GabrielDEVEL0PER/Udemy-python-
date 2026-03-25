1e59d010e94fad769a93a1588b3ca9fa0c6bc74c
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-03-25 09:29:37 -0300

    adiciona script para verificar o sexo informado pelo usuário

---
+++ pratica/ex16.py
@@ -0,0 +1,8 @@
+sexo = input('Digite F para feminino e M para masculino: ').upper()
+
+if sexo == 'F':
+    print('OK seu sexo é FEMININO')
+elif sexo == 'M':
+    print('Ok seu sexo é masculino')
+else:
+    print("Sexo invalido")
