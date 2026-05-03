0dec67ecf62d586e2cf979604d26c31653e1a16f
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-05-02 23:32:46 -0300

    adiciona script para saudação com base no turno do estudante

---
+++ pratica/ex23.py
@@ -0,0 +1,10 @@
+turno = input("Qual turno você estuda [M - Matutino] [V - Vespertino] [N - Noturno.]: ").upper()
+
+if turno == "M":
+    print("Bom dia!")
+elif turno == "V": 
+    print("Boa tarde!")
+elif turno == "N":
+    print("Boa noite!")
+else:
+    print("Turno invalido!")
