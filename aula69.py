8ffae446d5d0a846d2933e1c783d1e6a88b69018
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-05-27 22:53:29 -0300

    atualiza conteúdo do arquivo aula69.txt e ajusta escrita no script aula69.py

--- aula69.py
+++ aula69.py
@@ -5,8 +5,26 @@ caminho_arq = '/home/gabriel/Área de trabalho/Udemy/aula69.txt'
 
 # arquivo.close()
 
-with open(caminho_arq, 'w', encoding='utf-8') as arquivo:
-    arquivo.write('Ola mundo\n')
-    arquivo.write('arquivo vai ser fechado')
+with open(caminho_arq, 'w+') as arquivo:
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(
 
-print('O código rodou e o arquivo foi fechado com sucesso.')
+        ("Linha 3\n", 'Linha4\n')
+    )
+    arquivo.seek(0, 0)
+    print(arquivo.read())
+    print('Lendo')
+    arquivo.seek(0, 0)
+    print(arquivo.readline(), end='')
+    print(arquivo.readline().strip())
+    print(arquivo.readline().strip())
+
+    print('READLINES')
+    arquivo.seek(0, 0)
+    for linha in arquivo.readlines():
+        print(linha.strip())
+print('#' * 10)
+
+with open(caminho_arq, 'r') as arquivo: 
+    print(arquivo.read())
