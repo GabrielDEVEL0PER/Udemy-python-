611218948c03be860684622c6d2eb7e05937119e
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-06-02 18:29:18 -0300

    atualiza conteúdo do arquivo aula69.txt e ajusta escrita no script aula69.py

--- aula69.py
+++ aula69.py
@@ -1,3 +1,6 @@
+
+
+
 caminho_arq = '/home/gabriel/Área de trabalho/Udemy/aula69.txt'
 
 
@@ -5,26 +8,36 @@ caminho_arq = '/home/gabriel/Área de trabalho/Udemy/aula69.txt'
 
 # arquivo.close()
 
-with open(caminho_arq, 'w+') as arquivo:
-    arquivo.write('Linha 1\n')
+# with open(caminho_arq, 'w+') as arquivo:
+#     arquivo.write('Linha 1\n')
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(
+
+#         ("Linha 3\n", 'Linha4\n')
+#     )
+#     arquivo.seek(0, 0)
+#     print(arquivo.read())
+#     print('Lendo')
+#     arquivo.seek(0, 0)
+#     print(arquivo.readline(), end='')
+#     print(arquivo.readline().strip())
+#     print(arquivo.readline().strip())
+
+#     print('READLINES')
+#     arquivo.seek(0, 0)
+#     for linha in arquivo.readlines():
+#         print(linha.strip())
+# print('#' * 10)
+
+# with open(caminho_arq, 'r') as arquivo: 
+#     print(arquivo.read())
+
+
+
+with open(caminho_arq, 'w') as arquivo:
+    arquivo.write('Atenção\n')
     arquivo.write('Linha 2\n')
     arquivo.writelines(
 
         ("Linha 3\n", 'Linha4\n')
     )
-    arquivo.seek(0, 0)
-    print(arquivo.read())
-    print('Lendo')
-    arquivo.seek(0, 0)
-    print(arquivo.readline(), end='')
-    print(arquivo.readline().strip())
-    print(arquivo.readline().strip())
-
-    print('READLINES')
-    arquivo.seek(0, 0)
-    for linha in arquivo.readlines():
-        print(linha.strip())
-print('#' * 10)
-
-with open(caminho_arq, 'r') as arquivo: 
-    print(arquivo.read())
