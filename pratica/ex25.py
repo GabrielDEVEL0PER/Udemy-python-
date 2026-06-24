157cef855b7227bb1163ca2475832f499238c1d3
Gabriel de Aguiar Cordeiro <gabrielag.developer@gamil.com>
2026-06-24 14:34:03 -0300

    praticando classes

--- pratica/ex25.py
+++ pratica/ex25.py
@@ -1,8 +1,15 @@
-class carro:
+class Carro:
     def __init__(self, modelo, marca):
         self.modelo = modelo
         self.marca = marca
 
     def descricao (self):
         return f'Carro {self.modelo} {self.marca}'
-        
+    
+    def acelerar(self, velocidade):
+        return f'{self.marca} {self.modelo} está aceleradndo a {velocidade} km/h'
+
+
+carro = Carro(marca='Toyota',modelo='Corolla')
+print(carro.descricao())
+print(carro.acelerar(80))
