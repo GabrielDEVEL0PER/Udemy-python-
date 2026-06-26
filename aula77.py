3aeea1c561a817b64ebe5a6656941ac75fad73cf
Gabriel de Aguiar Cordeiro <gabrielag.developer@gamil.com>
2026-06-25 21:38:11 -0300

    adiciona funcionalidade para salvar dados de pessoas em um arquivo JSON

---
+++ aula77.py
@@ -0,0 +1,21 @@
+import json 
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+p1 = Pessoa('Gabriel, 20')
+p2 = Pessoa ('Ana, 25')
+
+
+dados = [
+    {'nome': p1.nome, 'idade': p1.idade},
+    {'nome': p2.nome, 'idade': p2.idade}
+]
+
+with open('pessoas.json', 'w',) as arquivo:
+    json.dumb(dados, arquivo)
+
+print('Dados Salvos')
+
