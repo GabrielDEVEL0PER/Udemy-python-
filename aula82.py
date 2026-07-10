00e9557c6c5fa96b90861567eb35abe562f35695
Gabriel de Aguiar Cordeiro <gabrielag.developer@gamil.com>
2026-07-09 21:57:58 -0300

    adiciona classe Foo com métodos de acesso público, protegido e privado

---
+++ aula82.py
@@ -0,0 +1,23 @@
+from functools import partial
+
+class Foo:
+    def __init__(self):
+        self.public = 'isso é publico'
+        self._protected = 'isso é protegido'
+        self.__exemplo = 'isso é privado'
+
+    def metodo_publico(self):
+        print(self.__exemplo)
+        self.__metodo_private()
+        return 'metodo_publico'
+
+    def __metodo_protected(self):
+        print('_metodo_proteced')
+        return '_metodo_protected'
+
+    def __metodo_private(self):
+        print('__metodo_private')
+        return '__metodo_private'
+
+f = Foo()
+print(f.metodo_publico())
