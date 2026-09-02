3624eb3f3796d1411caec92962deb74b6dbb2ccf
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-09-02 01:52:35 -0300

    adiciona classes Log, LogFileMixin e LogFilePrintMixin com métodos para manipulação de logs

---
+++ aula141/log.py
@@ -0,0 +1,23 @@
+class Log:
+    def log(self, msg):
+        raise NotImplementedError('Implemente o método log')
+
+    def log_error(self,msg):
+        return self.log(f'Error: {msg}')
+
+
+class LogFileMixin(Log):
+    def log(self, msg):
+        print(msg)
+
+
+
+class LogFilePrintMixin(Log):
+    def log(self, msg):
+        print(f'{msg} {self.__class__.__name__})
+
+    
+
+if __name__ == '__main__':
+    l = LogFileMixin()
+    l.log('qualquer coisa')
