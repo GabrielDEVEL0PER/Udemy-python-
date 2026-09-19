e1d999ec0c33bc5296d39be4739de4b592fcd1e2
Gabriel Aguiar <gabrielag.developer@gmail.com>
2026-09-19 02:52:21 -0300

    corrige a função imprimir_numero para exibir números em formato triangular

--- pratica/pratica27.py
+++ pratica/pratica27.py
@@ -1,8 +1,47 @@
 def imprimir_numero(n):
     for i in range(1, n + 1):
-        for j in range(i):
-            print(i, end='  ')
+        for j in range(1, i + 1):
+            print(j, end='  ')
         print()
 
 n = int(input("Digite um numero: "))
 imprimir_numero(n)
+
+# pratica---------//----------------
+# def imprimir_numero(n):
+#     for i in range(1, n  + 1):
+#         for j in range(1, i + 1):
+#             print(j, end=' ')
+#         print
+
+# def imprimir_numero(n):
+#     for i in range(1, n + 1):
+#         for j in range(1, i + 1):
+#             print(j, end=' ')
+#         print
+
+
+# def imprimir_numero(n):
+#     for i in range(1, n + 1):
+#         for j in range(1, i + 1):
+#             print(j, end='  ')
+#         print
+
+
+# def imprimir_numero(n):
+#     for i in range(1, n + 1):
+#         for j in range(1, i + 1):
+#             print(j, end= ' ')
+#         print
+
+# def imprimir_numero(n):
+#     for i in range (1, n + 1):
+#         for j in range(1, i + 1):
+#             print(j, end= ' ')
+#         print()
+
+# def imprimir_numero(n):
+#     for i in range(1, n + 1):
+#         for j in range(1, i + 1):
+#             print(j, end= ' ')
+#         print()
